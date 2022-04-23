a97723ad74700682bc9a30167b7e278d09b91541
NikaMVA <nika-mva@mail.ru>
2022-04-23 10:05:00 +0300

    вычислила факториал

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,29 @@
 function factorial(n) {
-  // ваш код...
+    let result = 1;
+  for(let i = 1; i<=n; i++){
+    result  = i*result;
+  }
+  return result;
+}
+
+function example() {
+    // for(let i = 0; i < 3; i++) {
+    //     console.log(i);
+    // }
+    let i = 0;
+    if (i < 3) {
+        console.log(i);
+    }
+    i++;
+    if (i < 3) {
+        console.log(i);
+    }
+    i++;
+    if (i < 3) {
+        console.log(i);
+    }
+    i++;
+    if (i < 3) {
+        console.log(i);
+    }
 }
