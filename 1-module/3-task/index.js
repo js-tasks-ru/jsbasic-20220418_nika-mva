058ef37951c1ce3aba6bd1172eecc03d03d14408
NikaMVA <nika-mva@mail.ru>
2022-04-26 20:37:01 +0300

    Сделлаь первый символ заглавным

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,10 @@
 function ucFirst(str) {
   // ваш код...
+  if (!str) {
+    return str;
+  }
+  const firstChar = str[0];
+  const apperChar = firstChar.toUpperCase();
+  const strTrim = str.slice(1);
+  return apperChar + strTrim;
 }
