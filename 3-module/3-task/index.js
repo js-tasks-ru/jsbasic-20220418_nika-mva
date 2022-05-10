4d54cbf36b6ee0a140fde2ffddc4192cd3928a2c
NikaMVA <nika-mva@mail.ru>
2022-05-10 20:45:51 +0300

    Перевела текст вида

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,22 @@
 function camelize(str) {
-  // ваш код...
+  function ucFirst(str) {
+    
+    if (!str) {
+      return str;
+    }
+    const firstChar = str[0];
+    const apperChar = firstChar.toUpperCase();
+    const strTrim = str.slice(1);
+    return apperChar + strTrim;
+  }
+
+  let name = str.split('-');
+  let arr = name.map(function(currentValue, index){
+    if (index === 0){
+      return currentValue;
+    }
+    return ucFirst(currentValue);
+  })
+  let result = arr.join('');
+  return result;
 }
